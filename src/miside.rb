ffe0efcb28a71083c9880e1ceeb0cbaf46a76aa7
Alexander Hoem Rosbach <aro037@student.uib.bo>
2012-09-03 21:54:32 +0200

    designing miside

--- src/miside.rb
+++ src/miside.rb
@@ -3,29 +3,47 @@
 require 'net/http'
 require 'uri'
 
-def fetch(uri_str, cookie = nil, limit = 10)
+LOGIN_URL = "https://miside.uib.no/register/user-login"
+
+def fetch(uri_str, limit = 10)
       # You should choose a better exception.
        raise ArgumentError, 'too many HTTP redirects' if limit == 0
     
        uri = URI.parse(uri_str)
        req = Net::HTTP::Get.new(uri.path)
-       req.initialize_http_header({'Cookie' => cookie}) unless cookie.nil?
+       req.initialize_http_header({'Cookie' => @cookie}) unless @cookie.nil?
 
        http = Net::HTTP.new(uri.host, uri.port)
        http.use_ssl = uri.scheme == 'https'
-       response = http.request(req)
+       yield http.request(req)
+end
 
+def followRedirect(response)
        case response
        when Net::HTTPSuccess then
            response
        when Net::HTTPRedirection then
            location = response['location']
            warn "redirected to #{location}"
-           fetch(location, cookie, limit - 1)
+           fetch(location, limit - 1)
        else
-           response.value
+           response
        end
 end
+
+def login(email, password)
+    uri = URI.parse(LOGIN_URL)
+    req = Net::HTTP::Post.new(uri.path)
+    req.form_data = {'email' => email, 'password' => password, 'domain' => 'student'}
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl= uri.scheme == 'https'
+    
+    response = http.request(req)
+    @cookie = response['Set-Cookie']
+    yield response
+end
+
 
 def post(uri_str, data, limit = 10)
       # You should choose a better exception.
@@ -51,7 +69,8 @@ def post(uri_str, data, limit = 10)
        end
 end
 
-puts post("https://miside.uib.no/register/user-login", {'email' => 'aro037@student.uib.no', 'password' => '', 'domain' => 'student'}).body
+
+#puts post("https://miside.uib.no/register/user-login", {'email' => 'aro037@student.uib.no', 'password' => '', 'domain' => 'student'}).body
 #puts fetch("https://miside.uib.no/dotlrn/classes/det-matematisk-naturvitenskapelige-fakultet/inf100/inf100-2012h/dotlrn-homework/folder-contents?folder_id=99855833&return_url=%2fdotlrn%2fclasses%2fdet-matematisk-naturvitenskapelige-fakultet%2finf100%2finf100-2012h%2fone-community%3fpage_num%3d0&homework_user_id=").body
 
 #uri = URI('https://miside.uib.no/register/user-login?locale=')
