b193c1a24be28e0d029d8831d370a6bd1260d9b2
mapster <mapster@ndt-lan.no>
2012-09-05 19:55:12 +0200

    Added menu to select start and end index, added some input validation.

--- src/main.rb
+++ src/main.rb
@@ -29,35 +29,113 @@ class MyMenu < Menu
     end
 
     def action_download
-       print "Path to store downloads: "
-       rootPath = gets.strip
+        submissions = fetch_submissions
 
-       print "Start index (zero-based): "
-       start = Integer(gets)
+        print "Path to store downloads: "
+        rootPath = gets.strip
 
-       print "Number of students: "
-       count = Integer(gets)
+        select_menu = SelectSubMenu.new(submissions)
+        puts "Select first student in range by: "
+        start_index = select_menu.display
+        puts "Selected index #{start_index} (#{submissions[start_index].student})"
 
-       submissions = fetch_submissions
-       last = ""
-       while count > 0 && start < submissions.size
-           s = submissions[start]
-           path = "#{rootPath}/#{s.student.sub(", ", ".")}"
-           FileUtils.mkdir_p(path) unless File.directory?(path)
-           puts "Downloading: #{s.student} <#{s.file_name}>"
+        select_menu = SelectSubMenu.new(submissions, start_index)
+        puts "Select last student in range by: "
+        end_index = select_menu.display
+        puts "Selected index #{start_index} (#{submissions[start_index].student})"
 
-           filepath = "#{path}/#{s.last_modified}-#{s.file_name}"
-           open(filepath, "wb") {|file|
-               file.write(@miside.fetch_submission(s.url).body)
-           }
+        last = ""
+        while start_index <= end_index && start_index < submissions.size
+            s = submissions[start_index]
+            path = "#{rootPath}/#{s.student.sub(", ", ".")}"
+            FileUtils.mkdir_p(path) unless File.directory?(path)
+            puts "Downloading: #{s.student} <#{s.file_name}>"
 
-           count -= 1 unless last == s.student
-           start += 1
-           last = s.student
-       end
+            filepath = "#{path}/#{s.last_modified}-#{s.file_name}"
+            open(filepath, "wb") {|file|
+                file.write(@miside.fetch_submission(s.url).body)
+            }
+
+            start_index += 1
+            last = s.student
+        end
     end
+
 end
 
+class SelectSubMenu < Menu
+    def initialize(submissions, first = 0)
+        super()
+        @submissions = submissions
+        @first = first
+    end
+
+    def action_by_index
+        print "Enter index: "
+        index = read_int
+        if index < @first || index >= @submissions.size 
+            puts "Invalid index, should be in interval [#{@first}, #{@submissions.size-1}]"
+        else
+            self.run!
+        end
+        index
+    end
+
+    def action_by_student_count
+        print "Enter student count (i.e the xth student): "
+        number = read_int
+        last = @submissions[0].student
+        index = 0
+        current = 1
+        while number > 1 && current < @submissions.size
+            s = @submissions[current]
+            if last != s.student
+                number -= 1 
+                index = current
+            end
+            current += 1
+            last = s.student
+        end
+        self.run!
+        index
+    end
+
+    def action_by_name
+        print "Enter name of first student (surname, firstname ...): "
+        name = gets.strip
+        result = search(name)
+        if result.size > 1
+            puts "#{result.size} results, be more precise."
+            result.each {|r|
+                puts r.student
+            }
+            action_by_name
+        elsif result.size == 0
+            puts "0 found."
+            action_by_name
+        elsif @submissions.index(result.first) < @first
+            puts "Found student has a lower index (#{@submissions.index(result.first)}) than the first (#{@first})"
+            action_by_name
+        else
+            self.run!
+            @submissions.index(result.first)
+        end
+    end
+
+    def search(name)
+        selected = Array.new
+        @submissions.select {|s| 
+            add = false
+            if s.student.start_with?(name) 
+                if not selected.include?(s.student)
+                    selected << s.student
+                    add = true
+                end
+            end
+            add 
+        }
+    end
+end
 print "Email: "
 email = gets.strip
 
@@ -71,12 +149,6 @@ puts "Url for submission folder: "
 submissionUrl = gets
 
 m = MyMenu.new(miside, submissionUrl)
-while true
-    m.print_menu
-    print "Select an action: "
-    m.call_action(Integer(gets))
-end
+m.display
 
 exit
-
-
--- src/menu.rb
+++ src/menu.rb
@@ -4,23 +4,56 @@ class Menu
 
     def initialize
         @actions = self.methods.select { |m| m.to_s.start_with?("action_") }
+        @run = true
+    end
+
+    def run?
+        @run
+    end
+
+    def run!
+        @run =  ! @run
     end
 
     def print_menu
-       menu = @actions.map {|a| a.to_s.sub("action_","").sub("_", " ").capitalize }
-       i = 1
-       menu.each {|m|
-           puts "#{i}. #{m}\n"
-           i += 1
-       }
+        menu = @actions.map {|a| a.to_s.sub("action_","").gsub("_", " ").capitalize }
+        i = 1
+        menu.each {|m|
+            puts "#{i}. #{m}\n"
+            i += 1
+        }
     end
 
     def action_quit
-       exit 
+        @run = false
+    end
+
+    def read_int
+        begin
+            Integer(gets.strip)
+        rescue
+            puts "Not a valid number."
+            read_int
+        end
+    end
+
+    def display
+        while run?
+            print_menu
+            print "Select action: "
+            choice = read_int
+            if choice > 0 && choice <= @actions.size
+                ret = call_action(choice)
+            else
+                puts "Invalid action number."
+            end
+            puts "---"
+        end
+        ret
     end
 
     def call_action(index)
-       self.send(@actions[index-1].to_sym) 
+        self.send(@actions[index-1].to_sym)
     end
 
 end
